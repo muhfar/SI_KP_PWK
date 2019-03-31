1bd0eb5460287f5afac26974082b4eb7e9d2759c
Ivena Chindy Claudia <37652346+ivenacindy@users.noreply.github.com>
2019-03-31 23:59:51 +0700

    Form Upload (Surat & Laporan KP)
    implementasi form upload surat pengajuan dan laporan KP dari high fidelity prototype. masih ada beberapa yang kurang rapi.
    halaman landing page (home) sudah dibuat akan tetapi belum selesai

---
+++ application/views/v_form_upload_laporan.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<title>SI KP PWK : Upload Surat Laporan</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+
+	<link rel="stylesheet" href="assets/css/style.css">
+	<style>
+		#Upload_tombol {
+		opacity: 1;
+		position: absolute;
+		left: 643px;
+		top: 515px;
+		box-sizing: border-box;
+		margin: 0;
+		padding: 0;
+		overflow: visible;
+		width: 60px;
+		white-space: nowrap;
+		text-align: center;
+		font-family: Poppins;
+		font-style: normal;
+		font-weight: bold;
+		font-size: 16px;
+		color: rgba(255,255,255,1);
+	}
+	</style>
+</head>
+
+<body>
+	
+
+	<div id="form_upload_pendaftaran">
+	<svg class="Rectangle_25">
+		<rect id="Rectangle_25" rx="6" ry="6" x="0" y="0" width="474" height="167">
+		</rect>
+	</svg>
+	<div id="Form_Upload_Pendaftaran_Kuliah">
+		<span>Form Upload<br/>Laporan Kuliah Praktik</span>
+	</div>
+	<div id="Berkas">
+		<span>Laporan</span>
+	</div>
+	<div id="drag_and_drop_or_click_here">
+		<span>drag and drop<br/>or click here</span>
+	</div>
+	<div id="_">
+		<span>:</span>
+	</div>
+	<svg class="Rectangle_17">
+		<rect id="Rectangle_17" rx="21.5" ry="21.5" x="0" y="0" width="176" height="43">
+		</rect>
+	</svg>
+	<div id="Upload_tombol">
+		<span>Upload</span>
+	</div>
+	
+
+	<?php $this->load->view('_header_login');?>
+</body>
+</html>
