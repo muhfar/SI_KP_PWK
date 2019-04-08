91b37a74ec1193e5aee4a95aaa886b596a9bc23a
Ivena Chindy Claudia <37652346+ivenacindy@users.noreply.github.com>
2019-04-09 01:26:02 +0700

    Update c_home.php

--- application/controllers/c_home.php
+++ application/controllers/c_home.php
@@ -3,6 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class c_home extends CI_Controller {
 	public function index()
 	{
-		$this->load->view('v_jadwal_seminar');
+		$this->load->view('v_home');
 	}
 }
