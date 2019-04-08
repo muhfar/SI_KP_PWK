67dd5c0e2429717bb52718be7acff405fbc3dbc4
Ivena Chindy Claudia <37652346+ivenacindy@users.noreply.github.com>
2019-04-09 01:24:42 +0700

    Update FrontEnd
    upload halaman FAQ, Info Instansi, Jadwal Seminar (masih perlu beberapa perbaikan untuk jadwal seminar)

--- application/views/v_info_instansi.php
+++ application/views/v_info_instansi.php
@@ -12,12 +12,58 @@
         <!--LOAD HEADER-->
         <?php $this->load->view('_header');?>
 
+        <title-page>Info Instansi</title-page>
+
         <!--LIST INSTANSI-->
-        <div class="">
-            
-        </div>
+        <section class="container-fluid">
+            <a href="#"><div class="instansi">
+                <icon-instansi></icon-instansi>
+                <title-instansi>Perusahaan A</title-instansi>
+            </div><a>
+
+            <a href="#"><div class="instansi" 
+                style="left:367px">
+                <icon-instansi></icon-instansi>
+                <title-instansi>Perusahaan B</title-instansi>
+            </div><a>
+
+            <a href="#"><div class="instansi"
+                style="left:686px">
+                <icon-instansi></icon-instansi>
+                <title-instansi>Perusahaan C</title-instansi>
+            </div><a>
+
+            <a href="#"><div class="instansi"
+                style="left:1005px">
+                <icon-instansi></icon-instansi>
+                <title-instansi>Perusahaan D</title-instansi>
+            </div><a>
+
+            <a href="#"><div class="instansi" style="top:544px">
+                <icon-instansi></icon-instansi>
+                <title-instansi>Perusahaan E</title-instansi>
+            </div><a>
+
+            <a href="#"><div class="instansi"
+                 style="left:367px; top:544px">
+                <icon-instansi></icon-instansi>
+                <title-instansi>Perusahaan F</title-instansi>
+            </div><a>
+
+            <a href="#"><div class="instansi"
+                style="left:686px; top:544px">
+                <icon-instansi></icon-instansi>
+                <title-instansi>Perusahaan G</title-instansi>
+            </div><a>
+
+            <a href="#"><div class="instansi"
+                style="left:1005px; top:544px">
+                <icon-instansi></icon-instansi>
+                <title-instansi>Perusahaan H</title-instansi>
+            </div><a>
+        </section>
 
         <!--FOOTER-->
-        <?php $this->load->view('_footer');?>
+        <!--<?php $this->load->view('_footer');?>-->
     </body>
-</html>>
+</html>
--- application/views/v_jadwal_seminar.php
+++ application/views/v_jadwal_seminar.php
@@ -12,12 +12,55 @@
         <!--LOAD HEADER-->
         <?php $this->load->view('_header');?>
 
+        <title-page>Jadwal Seminar Kerja Praktik</title-page>
+
         <!--JADWAL SEMINAR-->
-        <div class="">
-            
-        </div>
+        <!--ini masih belum selesaiiii-->
+        <table class="paleBlueRows">
+            <thead>
+                <tr>
+                    <th>Nama</th>
+                    <th>NIM</th>
+                    <th>Waktu</th>
+                    <th>Ruangan</th>
+                    <th>Penguji</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>cell1_1</td>
+                    <td>cell2_1</td>
+                    <td>cell3_1</td>
+                    <td>cell4_1</td>
+                </tr>
+                <tr>
+                    <td>cell1_2</td>
+                    <td>cell2_2</td>
+                    <td>cell3_2</td>
+                    <td>cell4_2</td>
+                </tr>
+                <tr>
+                    <td>cell1_3</td>
+                    <td>cell2_3</td>
+                    <td>cell3_3</td>
+                    <td>cell4_3</td>
+                </tr>
+                <tr>
+                    <td>cell1_4</td>
+                    <td>cell2_4</td>
+                    <td>cell3_4</td>
+                    <td>cell4_4</td>
+                </tr>
+                <tr>
+                    <td>cell1_5</td>
+                    <td>cell2_5</td>
+                    <td>cell3_5</td>
+                    <td>cell4_5</td>
+                </tr>
+            </tbody>
+        </table>
 
         <!--FOOTER-->
         <?php $this->load->view('_footer');?>
     </body>
-</html>>
+</html>
