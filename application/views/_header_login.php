405d769d274daf820a06d4bc424ee24b799da4c2
Ivena Chindy Claudia <37652346+ivenacindy@users.noreply.github.com>
2019-04-15 02:43:34 +0700

    Update link, login form, dropdown login header

--- application/views/_header_login.php
+++ application/views/_header_login.php
@@ -1,5 +1,5 @@
 <header class="site-header">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css')?>">
     <style>
         .nav {
             position: fixed;
@@ -26,6 +26,38 @@
                 text-decoration: none; }
             .nav li a:hover {
                 background-color: antiquewhite; }
+
+            .dropdown {
+                position: relative;
+                display: inline-block;
+                font-family: Poppins;
+                font-style: normal;
+                font-size: 19 px;
+                padding: 20px; }
+
+            .dropdown-content {
+                display: none;
+                position: absolute;
+                background-color: #f9f9f9;
+                min-width: 160px;
+                box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+                z-index: 1; }
+
+            .dropdown-content a {
+                color: black;
+                padding: 12px 16px;
+                text-decoration: none;
+                display: block; }
+
+            .dropdown-content a:hover {
+                background-color: #f1f1f1}
+
+            .dropdown:hover .dropdown-content {
+                display: block; }
+
+            .dropdown:hover .dropbtn {
+                background-color: #3e8e41; }
+                
         .container-fluid {
             max-width: 1600px;
             background-color: white; }
@@ -35,14 +67,19 @@
         <div class="site-logo" style="background-color: rgb(255, 255, 255); width: 100%;
             padding :0 5rem 0 5rem !important">
 
-            <a href="#"><img src="<?php echo base_url('assets/images/logo_itera.png')?>" 
+            <a href="<?php echo base_url();?>"><img src="<?php echo base_url('assets/images/logo_itera.png')?>" 
                 style="margin : 10;height: 70px; width: 58px; "></a>
                 
         </div>
 
         <ul class="nav" style="padding :0 5rem 0 5rem !important">
-            <li><a href="#">HOME</a></li>
-            <li><a href="#">UPLOAD</a></li>
+            <li><a href="<?php echo base_url();?>">HOME</a></li>
+            <li class="dropbtn"><button class="dropbtn">UPLOAD</button>
+                <div class="dropdown-content">
+                    <a href="#">Upload Surat Pengantar</a>
+                    <a href="#">Upload Laporan</a>
+                </div>
+            </li>
             <li><a href="#">FAQ</a></li>
             <li ><a href="#">SIGN OUT</a></li>
         </ul>
--- application/views/v_login.php
+++ application/views/v_login.php
@@ -6,18 +6,33 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
         <title>SI KP PWK</title>
-        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css')?>">
     </head>
     <body>
-        <!--LOAD HEADER-->
-        <?php $this->load->view('_header');?>
 
-        <!--FORM LOGIN-->
-        <div class="login">
-            
-        </div>
-
-        <!--FOOTER-->
-        <?php $this->load->view('_footer');?>
-    </body>
+		<div id="id01" >
+			<form class="modalcontent-animate" action="/action_page.php">
+				<div class= "modal">
+					<title-page style="left:0px">Sign In</title-page>
+					
+					<label for="uname">
+						<b type="normal"> Username : </b>
+					</label>
+					<input type="text" placeholder="Enter Your Email or Username" name="uname" required>
+					
+					<br></br>
+					
+					<label for="pass"> 
+						<b type="normal"> Password : </b>
+					</label>
+					<input type="password" placeholder="Enter Password" name="pass" required>
+					
+					<br></br>
+					
+					<button type="submit">
+						<b type="signin">Sign In</b>
+					</button>
+				</div>
+		</div>
+	</body>
 </html>>
