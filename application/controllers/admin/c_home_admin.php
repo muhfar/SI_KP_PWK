c2af9572d2e953d4cab1815b577f15eadf4b2b7a
Rahmad Nurhuda <33447750+rhmdnrhuda@users.noreply.github.com>
2019-05-06 17:40:32 +0700

    Update c_home_admin.php

--- application/controllers/admin/c_home_admin.php
+++ application/controllers/admin/c_home_admin.php
@@ -3,6 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class c_home_admin extends CI_Controller {
 	public function index()
 	{
-		$this->load->view('admin/v_home_admin');
+		$query['query'] = $this->db->select('*')->get('provinsi');
+		$this->load->view('admin/v_home_admin',$query);
 	}
 }
