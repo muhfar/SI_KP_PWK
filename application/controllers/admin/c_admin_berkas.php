87f63eb15753c8421bde63e4a69eaf9f69ef288a
AjipBimo <45142055+AjipBimo@users.noreply.github.com>
2019-05-06 00:27:07 -0700

    Berkas

--- application/controllers/admin/c_admin_berkas.php
+++ application/controllers/admin/c_admin_berkas.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class c_admin_berkas extends CI_Controller {
 	public function index()
 	{
-		$this->db->select('nama_pengguna,id_berkas,surat,laporan');
+		$this->db->select('nama_depan,nama_belakang,id_berkas,surat,laporan');
 		$this->db->from('berkas');
 		$this->db->join ('pengguna','NIM = id_berkas');
 		$query['query'] =  $this->db->get();
