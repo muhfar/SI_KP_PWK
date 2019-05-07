d97e5b5793104a94ef481d26601dd3243b7b6401
muhfar <36448231+muhfar@users.noreply.github.com>
2019-05-07 11:11:43 +0700

    Header admin
    - add link into href

--- application/config/routes.php
+++ application/config/routes.php
@@ -53,3 +53,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'c_home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+$route['admin'] = 'admin/c_home_admin';
